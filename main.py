32a982df53be3dc214d5b22c07ac58ed399cde84
Harsh Sharma <ichbinharsh@gmail.com>
2023-10-15 15:12:14 +0200

    Added the main.py script file

---
+++ main.py
@@ -0,0 +1,12 @@
+#===========================================================================
+#
+# This is the master file for LS-DYNA Python Toolkit called as "ls-py-dyna"
+#
+# To read in detail what all functions and operations a user can do on a LS-DYNA
+# Input or Outfile, please read the documentation here:
+#
+# Author> Harsh Sharma (https://github.com/harshar26)
+#===========================================================================
+
+import os
+import shutil
